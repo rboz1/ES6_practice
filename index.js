d26480a0b3f50884a7ed5b77bfcae85567628965
rboz1 <63253421+rboz1@users.noreply.github.com>
2021-07-06 19:34:18 -0400

    added menu toggle

---
+++ index.js
@@ -0,0 +1,27 @@
+//Toggles mobile hamburger menu on and off
+"use strict";
+let toggleMenu = () => {
+    let menu = document.getElementsByClassName("toggle-on");
+  
+    if (menu[0].style.display === "" || menu[0].style.display === "none"){
+      return menu[0].style.display = "flex";
+        }else if(menu[0].style.display = "flex"){
+         return menu[0].style.display = "none";}
+  };
+  
+
+  /*Prevents desktop navbar from disappearing after mobile hamburger menu is toggled 
+  and window is resized*/
+  
+//   let displayLargeNav = () => {
+//     let width = window.innerWidth;
+//     let menu = document.getElementsByClassName("toggle-menu");
+  
+//     if (width >= 752){
+//       return menu[0].style.display = "flex";
+//     }else{
+//       return menu[0].style.display = "none";
+//     }
+//   };
+  
+//   window.onresize = displayLargeNav;

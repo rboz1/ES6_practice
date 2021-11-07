ad1b82c744c47e59f3caa968e1f6c94d1219099f
rboz1 <63253421+rboz1@users.noreply.github.com>
2021-11-07 14:07:26 -0500

    desktop nav styling

--- index.js
+++ index.js
@@ -2,27 +2,9 @@
 "use strict";
 let toggleMenu = () => {
     let menu = document.getElementsByClassName("toggle-on");
-    let width = window.innerWidth;
-  
+      
     if (menu[0].style.display === "" || menu[0].style.display === "none"){
       return menu[0].style.display = "flex";
         }else if(menu[0].style.display = "flex"){
          return menu[0].style.display = "none";}
   };
-  
-
-  /*Prevents desktop navbar from disappearing after mobile hamburger menu is toggled 
-  and window is resized*/
-  
-//   let displayLargeNav = () => {
-//     let width = window.innerWidth;
-//     let menu = document.getElementsByClassName("toggle-menu");
-  
-//     if (width >= 752){
-//       return menu[0].style.display = "flex";
-//     }else{
-//       return menu[0].style.display = "none";
-//     }
-//   };
-  
-//   window.onresize = displayLargeNav;

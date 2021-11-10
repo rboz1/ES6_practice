3da0fd03c316186f53bcba3ae656f72d8328b1a7
rboz1 <63253421+rboz1@users.noreply.github.com>
2021-11-09 20:46:17 -0500

    testing out glide.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 //Toggles mobile hamburger menu on and off
 "use strict";
 let toggleMenu = () => {
     let menu = document.getElementsByClassName("toggle-on");
-      
+
     if (menu[0].style.display === "" || menu[0].style.display === "none"){
       return menu[0].style.display = "flex";
         }else if(menu[0].style.display = "flex"){

7135cd983414428073e211c8aed9efb9e36a96c8
rboz1 <63253421+rboz1@users.noreply.github.com>
2021-11-20 17:07:45 -0500

    arrow styling

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
     var splide = new Splide( '.splide', {
       drag: "true",
       classes: {
-        arrows: 'splide__arrows your-class-arrows',
-        arrow : 'splide__arrow your-class-arrow',
-        prev  : 'splide__arrow--prev your-class-prev',
-        next  : 'splide__arrow--next your-class-next',
+        arrows: 'splide__arrows angles-arrows',
+        arrow : 'splide__arrow angles-arrow',
+        prev  : 'splide__arrow--prev angles-prev',
+        next  : 'splide__arrow--next angles-next',
       },
     } );
     splide.mount();

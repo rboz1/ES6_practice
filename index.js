f17672eaccc90321d7074e522c8c4fee56312dc0
rboz1 <63253421+rboz1@users.noreply.github.com>
2021-11-27 15:42:17 -0500

    final tweaks to styling and js

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-
-  document.addEventListener( 'DOMContentLoaded', function() {
+//splide.js
+document.addEventListener( 'DOMContentLoaded', function() {
     var splide = new Splide( '.splide', {
       drag: "true",
       classes: {
-        arrows: 'splide__arrows angles-arrows',
-        arrow : 'splide__arrow angles-arrow',
-        prev  : 'splide__arrow--prev angles-prev',
-        next  : 'splide__arrow--next angles-next',
+        arrows: 'splide__arrows',
+        arrow : 'splide__arrow',
+        prev  : 'splide__arrow--prev',
+        next  : 'splide__arrow--next'
       },
     } );
     splide.mount();
   } );
 
-//Toggles mobile hamburger menu on and off
+//Toggles mobile hamburger menu on and off if window width is below 1200px
 "use strict";
 let toggleMenu = () => {
-    let menu = document.getElementsByClassName("toggle-on");
+  let menu = document.getElementsByClassName("toggle-on");
 
-    if (menu[0].style.display === "" || menu[0].style.display === "none"){
-      return menu[0].style.display = "flex";
-        }else if(menu[0].style.display = "flex"){
-         return menu[0].style.display = "none";}
+  if (menu[0].style.display === "" || menu[0].style.display === "none"){
+    return menu[0].style.display = "flex";
+  }else if(menu[0].style.display = "flex"){
+    return menu[0].style.display = "none";}
+};
+
+
+//Prevents desktop navbar from disappearing after mobile hamburger menu is toggled on and window is resized
+  let displayLargeNav = () => {
+    let width = window.innerWidth;
+    let menu = document.getElementsByClassName("toggle-on");
+  
+    if (width >= 1200){
+      return menu[0].style.display = "none";
+    }
   };
+  
+  window.onresize = displayLargeNav;
